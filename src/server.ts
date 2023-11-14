93c43e11ffc952b546111fd77dc70cdc9bbbf022
Luiz <luizinhon@hotmail.com>
2023-11-13 22:33:05 -0300

    deploy config

--- src/server.ts
+++ src/server.ts
@@ -11,14 +11,11 @@ app.register(cors, {
 });
 app.register(routes);
 
-const baseUrl = process.env.BASE_URL
-	? process.env.BASE_URL
-	: 'http://localhost';
-const port = process.env.PORT ? Number(process.env.PORT) : 3333;
 app
 	.listen({
-		port,
+		host: '0.0.0.0',
+		port: process.env.PORT ? Number(process.env.PORT) : 3333,
 	})
 	.then(() => {
-		console.log(`HTTP server running on ${baseUrl}:${port}`);
+		console.log('HTTP server running');
 	});
